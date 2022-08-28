11d77435458a19f2732a330620f6a9a646f16ef2
Sergio Mesa <davidmesab7@gmail.com>
2022-08-28 10:57:47 -0500

    add other folder method

---
+++ Metodos/Metodos3/Metodos3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1 = 7;
+            double valor2 = 5.2;
+            double valor3 = 8.3;    
+
+            Console.WriteLine(Suma(valor1,valor2));
+            Console.ReadKey();
+        }
+
+        static double Suma(int num1, double num2, double num3=0)
+        {
+            return num1+num2+num3;
+        }
+
+        static double Suma(int num1, double num2 )
+        {
+            return num1 + num2; 
+        }
+
+    }
+}
