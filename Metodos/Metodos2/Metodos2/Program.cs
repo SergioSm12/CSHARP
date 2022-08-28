9a5290eb38e9236a6c0a8a1114dec3dbe24c32a1
Sergio Mesa <davidmesab7@gmail.com>
2022-08-27 22:15:13 -0500

    Add other method

---
+++ Metodos/Metodos2/Metodos2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese numero 1 : ");
+            int numero1 = int.Parse(Console.ReadLine());    
+            Console.Write("Ingrese numero 2 : ");
+            int numero2=int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(SumaNumeros(numero1, numero2));
+
+            Console.ReadKey();
+        }
+
+        static int SumaNumeros(int numero1, int numero2)
+        {
+            int resultado = numero1+ numero2;
+            return resultado;
+        }
+    }
+}
