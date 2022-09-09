8400f7829923954b51a2be8ddc1da7cfaadcd0ee
Sergio Mesa <davidmesab7@gmail.com>
2022-09-08 20:05:48 -0500

    add Herencia

---
+++ Arrays/ArrayDevuelveArray/ArrayDevuelveArray/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDevuelveArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayElements = LeerDatos();
+
+            foreach (int element in arrayElements) Console.WriteLine(element);
+            int suma = SumaArrays(arrayElements);
+
+            Console.WriteLine("La suma de los elementos del array es : " + suma);
+
+            Console.ReadKey();
+
+        }
+
+        static int[] LeerDatos()
+        {
+            Console.WriteLine("Cuantos elementos quieres que tenga el array : ");
+            string tamaño = Console.ReadLine();
+
+            int numElementos = int.Parse(tamaño);
+
+            int[] datos = new int[numElementos];
+
+            for (int i = 0; i < numElementos; i++)
+            {
+                Console.Write($"Introduzca el dato para la posicion {i} : ");
+                tamaño = Console.ReadLine();
+
+                int datosRelleno = int.Parse(tamaño);
+
+                datos[i] = datosRelleno;
+            }
+            return datos;
+        }
+
+        static int SumaArrays(int[] datos)
+        {
+            int suma = 0;
+            for(int i=0; i<datos.Length; i++)
+            {
+                suma+=datos[i];
+            }
+
+            return suma;
+        }
+
+    }
+}
---
+++ POO/Herencia/Herencia1/Herencia1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caballo  Babieca = new Caballo("Babieca");
+
+            Humano Juan = new Humano("Juan");
+
+            Gorila Copito = new Gorila("Copito");
+
+
+            Mamiferos animal = new Caballo("Bucefalo"); //Principio de sustitucion 
+                                                        //Ejemplo de uso de principio de sustitucion con array de clases
+            Mamiferos[] almacenAnimales = new Mamiferos[3];
+
+            almacenAnimales[0] = Babieca;
+            almacenAnimales[1] = Juan;
+            almacenAnimales[2] = Copito;
+            //En este caso solo se accede a metodos de la superclase Mamiferos 
+            almacenAnimales[1].getNombre();
+
+
+            Console.ReadKey();
+        }
+    }
+
+    class Mamiferos
+    {
+
+        private String nombreSerVivo;
+
+        public Mamiferos(string nombreSerVivo)
+        {
+            this.nombreSerVivo = nombreSerVivo;
+        }
+
+        public void respirar()
+        {
+            Console.WriteLine("Soy capaz de respirar");
+        }
+
+        public void cuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crias hasta que se valgan por si solas");
+        }
+
+        public void getNombre()
+        {
+            Console.WriteLine("El nombre del ser vivo es " + nombreSerVivo);
+        }
+    }
+
+    class Caballo : Mamiferos
+    {
+        public Caballo(string nombreCaballo):base(nombreCaballo)
+        {
+
+
+        }
+        public void galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar");
+        }
+
+
+    }
+
+    class Humano : Mamiferos
+    {
+
+        public Humano(string nombreHumano) : base(nombreHumano)
+        {
+
+        }
+        public void pensar()
+        {
+            Console.WriteLine("Soy capaz de pensar ");
+        }
+    }
+
+    class Gorila : Mamiferos 
+    {
+
+        public Gorila(string nombreGorila) : base(nombreGorila)
+        {
+
+        }
+      public void trepar()
+        {
+            Console.WriteLine("soy capaz de treapar");
+        }
+    }
+}
